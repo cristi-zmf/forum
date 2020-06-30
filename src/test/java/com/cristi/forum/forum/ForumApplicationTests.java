6ed1d61a9ed8ce6c39bc036ead978cec4dd0f96a
cristi-zmf <cristi-zmf>
2020-06-30 20:07:56 +0300

    First

---
+++ src/test/java/com/cristi/forum/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristi.forum.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
